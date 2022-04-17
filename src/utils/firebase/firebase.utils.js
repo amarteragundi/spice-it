60e4bf9d67a6e3f71e04577c8e627921f794d38e
Amar Teragundi <amarteragundi@gmail.com>
2022-04-17 14:16:32 +0530

    create and store document

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -27,27 +27,27 @@ provider.setCustomParameters({
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 
-// export const db = getFirestore();
+export const db = getFirestore();
 
-// export const createUserDocumentFromAuth = async (userAuth) => {
-//   const userDocRef = doc(db, 'users', userAuth.uid);
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
 
-//   const userSnapshot = await getDoc(userDocRef);
+  const userSnapshot = await getDoc(userDocRef);
 
-//   if (!userSnapshot.exists()) {
-//     const { displayName, email } = userAuth;
-//     const createdAt = new Date();
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
 
-//     try {
-//       await setDoc(userDocRef, {
-//         displayName,
-//         email,
-//         createdAt,
-//       });
-//     } catch (error) {
-//       console.log('error creating the user', error.message);
-//     }
-//   }
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
 
-//   return userDocRef;
-// };
+  return userDocRef;
+};
